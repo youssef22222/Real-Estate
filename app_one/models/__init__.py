ed49fb66d27b0621635a8d90d5b2f441aed6d60b
youssef22222 <youssef.elsayed0111@gmail.com>
2025-01-29 14:25:51 +0200

    Adding the property model and adding the basic filds in it

--- app_one/models/__init__.py
+++ app_one/models/__init__.py
@@ -2,3 +2,4 @@ from . import model_a
 from . import model_b
 from . import model_c
 from . import model_d
+from . import property
---
+++ app_one/models/property.py
@@ -0,0 +1,22 @@
+from odoo import models,fields
+
+class Property(models.Model):
+    _name = 'property'
+    name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float()
+    bedrooms = fields.Integer()
+    leaving_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation=fields.Selection([
+      #(name in database ,name that apear for the end user)
+        ("north","North"),
+        ("south","South"),
+        ("east","East"),
+        ("west","West")
+    ])
