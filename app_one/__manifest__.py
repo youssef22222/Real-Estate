7431d75a6fd61164c2dc0823977ce7b768360ec6
youssef22222 <youssef.elsayed0111@gmail.com>
2025-02-11 20:28:26 +0200

    Adding custom Font and using it in property report

--- app_one/__manifest__.py
+++ app_one/__manifest__.py
@@ -24,7 +24,10 @@
     'assets':{
         'web.assets_backend':[
             'app_one/static/src/css/property.css'
-        ]
+        ],
+        'web.report_assets_common': [
+            'app_one/static/src/css/font.css'
+        ],
     }
     ,
     'application': True,
