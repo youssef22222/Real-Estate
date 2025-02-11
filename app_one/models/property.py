e0edfe6f652c2bdb64304a72fc2ae9d24792f0b3
youssef22222 <youssef.elsayed0111@gmail.com>
2025-02-11 08:27:26 +0200

    Adding a smart button to open the property owner form from the property form view

--- app_one/models/property.py
+++ app_one/models/property.py
@@ -222,6 +222,12 @@ class Property(models.Model):
         #   this means (name != 'property 1' and state == 'draft')
         print(self.env['property'].search(search_domain))
 
+    def action_open_related_owner(self):
+        action = self.env['ir.actions.actions']._for_xml_id('app_one.owner_action')
+        view_id = self.env.ref('app_one.owner_view_form').id
+        action['res_id'] = self.owner_id.id
+        action['views'] = [[view_id,'form']]
+        return action
 
 class PropertyLine(models.Model):
     _name = 'property.line'
