0b2b224f130fa99ad756e3980a480b763180205d
youssef22222 <youssef.elsayed0111@gmail.com>
2025-02-12 17:01:36 +0200

    Creating API(Test EndPoint) => (http://localhost:8069/api/test)

---
+++ app_one/controllers/test_api.py
@@ -0,0 +1,8 @@
+from odoo import http
+
+
+class TestAPI(http.Controller):
+
+    @http.route("/api/test", methods=["GET"], type="http", auth="none", csrf=False)
+    def test_endpoint(self):
+        print("inside test_endpoint method")
