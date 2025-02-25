f5fdfb72c2f498815855d2f3fa870884f105616e
youssef22222 <youssef.elsayed0111@gmail.com>
2025-02-25 08:25:33 +0200

    Replacing ORM with RPC(Remote Procedure Call or Action Call) in ListViewAction to enable remote data fetching

--- app_one/static/src/components/listView/listView.js
+++ app_one/static/src/components/listView/listView.js
@@ -13,16 +13,28 @@ export class ListViewAction extends Component {
           "records" : []
       });
       this.orm = useService("orm");
-        this.loadRecords();
+      this.rpc = useService("rpc");
+      this.loadRecords();
     };
 
 
-    async loadRecords(){
-        const result = await this.orm.searchRead("property",[],[]);
-        //[],[] => domain and fields names to get
-        console.log(result);
-        this.state.records = result;
-    };
+    // async loadRecords(){
+    //     const result = await this.orm.searchRead("property",[],[]);
+    //     //[],[] => domain and fields names to get
+    //     console.log(result);
+    //     this.state.records = result;
+    // };
+
+       async loadRecords(){
+           const result = await this.rpc("/web/dataset/call_kw/",{
+               model : "property",
+               method : "search_read",
+               args : [[]], //search domain
+               kwargs : {fields:["id", "name", "postcode", "date_availability"]} //get only this fields
+           });
+           console.log(result);
+           this.state.records = result;
+       };
 }
 
 registry.category("actions").add("app_one.action_list_view",ListViewAction);
