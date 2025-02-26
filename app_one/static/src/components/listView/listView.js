e57d8d76544c6486ba13d369c7a3be0f1b34816b
youssef22222 <youssef.elsayed0111@gmail.com>
2025-02-26 09:43:30 +0200

    Updating ListView automatically in real-time every 3s with Component Rendering(setInterval()), stopped Rendering by onWillUnmount to avoid Error: Component is destroyed

--- app_one/static/src/components/listView/listView.js
+++ app_one/static/src/components/listView/listView.js
@@ -1,6 +1,6 @@
 /* @odoo-module */
 
-import { Component, useState } from "@odoo/owl";
+import { Component, useState, onWillUnmount } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 
@@ -15,6 +15,12 @@ export class ListViewAction extends Component {
       this.orm = useService("orm");
       this.rpc = useService("rpc");
       this.loadRecords();
+
+      // this.loadRecords() method will be called each 3 seconds to update the state automatically in real-time
+      this.intervalId = setInterval(() => {this.loadRecords();}, 3000);
+      // once you close or leave you page(component) this line will be executed to avoid Error: Component is destroyed
+      onWillUnmount(() => {clearInterval(this.intervalId);});
+
     };
 
 
